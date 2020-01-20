72b2093ff6b0ecda9874084a6b4a2aa7aae14899
juan12356789 <juancaph2015@gmail.com>
2020-01-20 11:23:29 -0600

    BUSQUEDA DE CLIENTES

--- src/routes/ventas.js
+++ src/routes/ventas.js
@@ -7,19 +7,17 @@ const multer=require("multer");
 const pool = require('../database');
 let orden; 
 
-const  rutimage=path.join(__dirname,"..","files"); 
+const  rutimage=path.join(__dirname,"..","files");
 
-
-  const storage=multer.diskStorage({
-    destination:function (res,file,cb) {
-      cb(null,rutimage)
-    },
-    filename:function (res,file,cb) {
-      console.log(file.originalname);
-
-      cb(null,Date.now()+file.originalname); 
-    }
-  }); 
+const storage=multer.diskStorage({
+   destination:function (res,file,cb) {
+     cb(null,rutimage)
+   },
+   filename:function (res,file,cb) {
+     console.log(file.originalname);
+     cb(null,Date.now()+file.originalname); 
+   }
+ }); 
 
   const  upload=multer({storage:storage}); 
 
@@ -28,22 +26,31 @@ const  rutimage=path.join(__dirname,"..","files");
     res.render('links/formularioVentas');
   });
   
-  router.get('/busqueda/:id',(req,res)=>{
-      const {id} =  req.params  ;
-     console.log(id);
-      res.render('links/formularioVentas',{nombre:id});
-  });
 
   router.post("/add",upload.array('gimg', 12),function (req,res) {
     let data = req.body; 
     res.render('links/formularioVentas'); 
     
   });
-  router.get("/busquedaCliente",async(req,res)=>{
-      const  info = await pool.query("SELECT * FROM clientes");
+  router.get('/busqueda/:id',async(req,res)=>{
+    const {id} =  req.params;
+    console.log(id);
+    const cliente  = await  pool.query('select nombre from clientes where idcliente = ?',[id]);
+    console.log(cliente);
+    res.render('links/formularioVentas',{cliente});
+});
+
+router.get("/busquedaCliente",async(req,res)=>{
+      const  info = await pool.query("SELECT * FROM clientes ORDER BY nombre ASC");
       console.log(info);
       res.render('links/busquedaClientes',{info}); 
-  }); 
+}); 
+router.post("/busquedaCliente", async(req,res)=>{
+  const {search} =  req.body; 
+  const  info  = await pool.query("SELECT * FROM clientes  where  nombre like ?",'%'+search+'%');
+  res.render('links/busquedaClientes',{info}); 
+  
+}); 
 
 
 module.exports = router; 
