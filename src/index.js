67502a6f3090b62d142c1b1094f265ab0b9adfaa
rosalizethestrada <up170043@alumnos.upa.edu.mx>
2020-01-27 11:18:14 -0600

    archivo

--- src/index.js
+++ src/index.js
@@ -54,6 +54,7 @@ app.use((req,res,next)=>{// se usa para ver que variable son accedidadas desde l
 app.use(require('./routes/login'));
 app.use('/ventas',require('./routes/ventas'));
 
+
 // Public 
 app.use(express.static(path.join(__dirname,'public')));
 // Starting server 
